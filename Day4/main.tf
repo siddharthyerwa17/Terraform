f4be2797189713b9a15ac1edbabe028fb39bce7a
Siddharth Yerwa <133352983+siddharthyerwa17@users.noreply.github.com>
2024-01-09 22:41:07 +0530

    Update main.tf

--- Day4/main.tf
+++ Day4/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "Server1" {
   instance_type = "t2.micro"
   ami = "ami-053b0d53c279acc90" # change this
   subnet_id = "subnet-019ea91ed9b5252e7" # change this
